5c1263954bb4bc81f5b8af488fb4f8eaa692aea4
Jcward31 <91399744+Jcward31@users.noreply.github.com>
2021-12-12 15:24:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// adding the api
+
+// when searching city name presented with city name, the date, weather condotions and UV index
+
+// uv indez when clicked shows color that indicates  weather con favoriabilty favorable, moderate or severe
+
+// view future to see html for 5 day forcast 
