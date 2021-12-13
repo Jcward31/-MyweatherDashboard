7bb5eaeaa23cc1028978252b20be3a7342b6d8e5
Jcward31 <91399744+Jcward31@users.noreply.github.com>
2021-12-12 20:52:22 -0500

    Add files via upload
    trying to make the api work

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 // adding the api
 
+var CitySrch = document.getElementById("Search");
+var weatherKey = "0b6b1584b248fb47b4145903c21533f1";
+
+CitySrch.addEventListener('click', event =>{
+    
+    var city =document.getElementById("SearchByCity").value;
+    console.log(city);
+    var currWeatherURl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherKey;
+ 
+console.log(currWeatherURl);
+
+    $.ajax({
+        url:currWeatherURl,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    });
+
+});
+
+
+
+
+
 // when searching city name presented with city name, the date, weather condotions and UV index
 
 // uv indez when clicked shows color that indicates  weather con favoriabilty favorable, moderate or severe
